208586ac7c9238adecab5cd81b9628f683af904c
Patrick Ziegler <patrick.a.ziegler@gmail.com>
2016-07-15 09:01:25 +0200

    added map api key

--- client/app/components/myMap/myMap.controller.js
+++ client/app/components/myMap/myMap.controller.js
@@ -1,6 +1,7 @@
 class MyMapController {
   constructor() {
     this.name = 'myMap';
+    this.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBbS0TVtTSSBiIup-LahxxY773efpz6qFA';
   }
 }
 
